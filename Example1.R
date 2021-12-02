fe9e43a9538de9a2318f0bf59485e8a9ef68e3f0
MICHAEL J ZENZ <zenz@wisc.edu>
2021-12-01 23:54:47 -0600

    Added read excel data file.

--- Example1.R
+++ Example1.R
@@ -1,13 +1,18 @@
 ###This script loads example survey data, finds means, medians, etc., and employs a number
 ##of inferential statistics
+
+#library(surveydata)
 #library(xlsxjars)
-library(xlsx)
+#library(xlsx)
 library(readxl)
 library(tidyverse)
+
+#This writes out the data file - column headers changed in excel.
 #data <- data.frame(membersurvey[,c("id","Q1_1", "Q1_2","Q4", "Q5","Q9_2","Q12_1","Q32", "Q35")])
 #write.csv(data, file="data.csv")
 #write.xlsx(data, "data.xlsx", col.names = TRUE, row.names = FALSE)
 
+data <- read_excel("data.xlsx")
 cor(data$`Years as Employee`, data$`Years as Independent`, use = "complete.obs")
 plot(data$`Years as Employee`, data$`Years as Independent`)
 years.lm <- lm(`Years as Independent` ~ `Years as Employee`  , data = data)
